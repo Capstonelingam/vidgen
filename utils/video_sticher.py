fe96b963b40ac70064a863f8a9a837a329d0d774
ramselvaraj <ramsevlaraj01@gmail.com>
2023-08-28 04:25:11 +0000

    Fixed Noise Issue in video_sticher

--- utils/video_sticher.py
+++ utils/video_sticher.py
@@ -2,7 +2,27 @@ from moviepy.editor import *
 import os
 from natsort import natsorted
 
-
+def concatenate(output_folder,video_clip_paths, output_path, method="compose"):
+    """Concatenates several video files into one video file
+    and save it to `output_path`. Note that extension (mp4, etc.) must be added to `output_path`
+    `method` can be either 'compose' or 'reduce':
+        `reduce`: Reduce the quality of the video to the lowest quality on the list of `video_clip_paths`.
+        `compose`: type help(concatenate_videoclips) for the info"""
+    # create VideoFileClip object for each video file
+    clips = [VideoFileClip(f"{output_folder}/{c}") for c in video_clip_paths]
+    if method == "reduce":
+        # calculate minimum width & height across all clips
+        min_height = min([c.h for c in clips])
+        min_width = min([c.w for c in clips])
+        # resize the videos to the minimum
+        clips = [c.resize(newsize=(min_width, min_height)) for c in clips]
+        # concatenate the final video
+        final_clip = concatenate_videoclips(clips)
+    elif method == "compose":
+        # concatenate the final video with the compose method provided by moviepy
+        final_clip = concatenate_videoclips(clips, method="compose")
+    # write the output video file
+    final_clip.write_videofile(output_path)
 
 def stitch_video(output_folder):
     L = []
@@ -11,14 +31,8 @@ def stitch_video(output_folder):
 
         #files.sort()
         files = natsorted(files)
-        for file in files:
-            if os.path.splitext(file)[1] == '.mp4':
-                filePath = os.path.join(root, file)
-                video = VideoFileClip(filePath)
-                L.append(video)
 
-    final_clip = concatenate_videoclips(L)
-    final_clip.to_videofile("../output/concat.mp4", fps=24, remove_temp=False)
+        concatenate(output_folder,files, f"{output_folder}/concat.mp4", method="compose")
 
 
 if __name__ == "__main__":
